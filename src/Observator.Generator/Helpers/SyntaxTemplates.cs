a826fae478278e5d3094c3169d54098116558c9c
Pavel Simsa <pavel@simsa.cz>
2025-06-29 17:18:34 +0100

    cleanup

--- src/Observator.Generator/Helpers/SyntaxTemplates.cs
+++ src/Observator.Generator/Helpers/SyntaxTemplates.cs
@@ -7,56 +7,95 @@ namespace Observator.Generator.Helpers
     internal static class SyntaxTemplates
     {
         // Using Directives
-        public static UsingDirectiveSyntax SystemUsing => SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System"));
-        public static UsingDirectiveSyntax SystemDiagnosticsUsing => SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Diagnostics"));
-        public static UsingDirectiveSyntax SystemRuntimeCompilerServicesUsing => SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Runtime.CompilerServices"));
+        public static readonly UsingDirectiveSyntax SystemUsing =
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System"));
+
+        public static readonly UsingDirectiveSyntax SystemDiagnosticsUsing =
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Diagnostics"));
+
+        public static readonly UsingDirectiveSyntax SystemRuntimeCompilerServicesUsing =
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Runtime.CompilerServices"));
 
         // Common SyntaxTokens
-        public static SyntaxToken PublicKeyword => SyntaxFactory.Token(SyntaxKind.PublicKeyword);
-        public static SyntaxToken StaticKeyword => SyntaxFactory.Token(SyntaxKind.StaticKeyword);
-        public static SyntaxToken FileKeyword => SyntaxFactory.Token(SyntaxKind.FileKeyword);
-        public static SyntaxToken SealedKeyword => SyntaxFactory.Token(SyntaxKind.SealedKeyword);
-        public static SyntaxToken InternalKeyword => SyntaxFactory.Token(SyntaxKind.InternalKeyword);
-        public static SyntaxToken PartialKeyword => SyntaxFactory.Token(SyntaxKind.PartialKeyword);
-        public static SyntaxToken AsyncKeyword => SyntaxFactory.Token(SyntaxKind.AsyncKeyword);
-        public static SyntaxToken ThisKeyword => SyntaxFactory.Token(SyntaxKind.ThisKeyword);
+        public static SyntaxToken PublicKeyword = SyntaxFactory.Token(SyntaxKind.PublicKeyword);
+        public static SyntaxToken StaticKeyword = SyntaxFactory.Token(SyntaxKind.StaticKeyword);
+        public static SyntaxToken FileKeyword = SyntaxFactory.Token(SyntaxKind.FileKeyword);
+        public static SyntaxToken SealedKeyword = SyntaxFactory.Token(SyntaxKind.SealedKeyword);
+        public static SyntaxToken InternalKeyword = SyntaxFactory.Token(SyntaxKind.InternalKeyword);
+        public static SyntaxToken PartialKeyword = SyntaxFactory.Token(SyntaxKind.PartialKeyword);
+        public static SyntaxToken AsyncKeyword = SyntaxFactory.Token(SyntaxKind.AsyncKeyword);
+        public static SyntaxToken ThisKeyword = SyntaxFactory.Token(SyntaxKind.ThisKeyword);
 
         // Type Names
-        public static TypeSyntax AttributeTypeName => SyntaxFactory.ParseTypeName("Attribute");
-        public static TypeSyntax ExceptionTypeName => SyntaxFactory.ParseTypeName("Exception");
-        public static TypeSyntax VarTypeName => SyntaxFactory.ParseTypeName("var");
+        public static readonly TypeSyntax AttributeTypeName = SyntaxFactory.ParseTypeName("Attribute");
+        public static readonly TypeSyntax ExceptionTypeName = SyntaxFactory.ParseTypeName("Exception");
+        public static readonly TypeSyntax VarTypeName = SyntaxFactory.ParseTypeName("var");
 
         // Attributes
-        public static AttributeSyntax AttributeUsageAttribute => SyntaxFactory.Attribute(SyntaxFactory.ParseName("AttributeUsage"));
-        public static AttributeArgumentSyntax AttributeTargetsMethodArgument => SyntaxFactory.AttributeArgument(SyntaxFactory.ParseExpression("AttributeTargets.Method"));
-        public static AttributeArgumentSyntax AllowMultipleTrueArgument => SyntaxFactory.AttributeArgument(SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression)).WithNameEquals(SyntaxFactory.NameEquals(SyntaxFactory.IdentifierName("AllowMultiple")));
-        public static AttributeSyntax InterceptsLocationAttribute => SyntaxFactory.Attribute(SyntaxFactory.ParseName("InterceptsLocation"));
+        public static readonly AttributeSyntax AttributeUsageAttribute =
+            SyntaxFactory.Attribute(SyntaxFactory.ParseName("AttributeUsage"));
+
+        public static readonly AttributeArgumentSyntax AttributeTargetsMethodArgument =
+            SyntaxFactory.AttributeArgument(SyntaxFactory.ParseExpression("AttributeTargets.Method"));
+
+        public static readonly AttributeArgumentSyntax AllowMultipleTrueArgument = SyntaxFactory
+            .AttributeArgument(SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression))
+            .WithNameEquals(SyntaxFactory.NameEquals(SyntaxFactory.IdentifierName("AllowMultiple")));
+
+        public static readonly AttributeSyntax InterceptsLocationAttribute =
+            SyntaxFactory.Attribute(SyntaxFactory.ParseName("InterceptsLocation"));
 
         // Expressions
-        public static LiteralExpressionSyntax TrueLiteralExpression => SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression);
-        public static LiteralExpressionSyntax StringLiteralExpression(string value) => SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(value));
-        public static LiteralExpressionSyntax NumericLiteralExpression(int value) => SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(value));
-        public static IdentifierNameSyntax ActivityIdentifierName => SyntaxFactory.IdentifierName("Activity");
-        public static IdentifierNameSyntax CurrentIdentifierName => SyntaxFactory.IdentifierName("Current");
-        public static IdentifierNameSyntax SourceIdentifierName => SyntaxFactory.IdentifierName("Source");
-        public static IdentifierNameSyntax StartActivityIdentifierName => SyntaxFactory.IdentifierName("StartActivity");
-        public static IdentifierNameSyntax ExceptionIdentifierName => SyntaxFactory.IdentifierName("exception");
-        public static IdentifierNameSyntax ThisSourceIdentifierName => SyntaxFactory.IdentifierName("@source");
-        public static IdentifierNameSyntax VariableDeclaratorName(string name) => SyntaxFactory.IdentifierName(name);
+        public static LiteralExpressionSyntax StringLiteralExpression(string value) =>
+            SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(value));
+
+        public static LiteralExpressionSyntax NumericLiteralExpression(int value) =>
+            SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(value));
+
+        public static readonly IdentifierNameSyntax ActivityIdentifierName = SyntaxFactory.IdentifierName("Activity");
+        public static readonly IdentifierNameSyntax CurrentIdentifierName = SyntaxFactory.IdentifierName("Current");
+        public static readonly IdentifierNameSyntax SourceIdentifierName = SyntaxFactory.IdentifierName("Source");
+
+        public static readonly IdentifierNameSyntax StartActivityIdentifierName =
+            SyntaxFactory.IdentifierName("StartActivity");
+
+        public static readonly IdentifierNameSyntax ThisSourceIdentifierName = SyntaxFactory.IdentifierName("@source");
 
         // Member Access
-        public static MemberAccessExpressionSyntax SimpleMemberAccessExpression(ExpressionSyntax expression, SimpleNameSyntax name) => SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, expression, name);
-        public static MemberAccessExpressionSyntax ActivityCurrentMemberAccess => SimpleMemberAccessExpression(ActivityIdentifierName, CurrentIdentifierName);
-        public static ConditionalAccessExpressionSyntax ActivitySourceConditionalAccess => SyntaxFactory.ConditionalAccessExpression(ActivityCurrentMemberAccess, SyntaxFactory.MemberBindingExpression(SourceIdentifierName));
-        public static MemberAccessExpressionSyntax ActivityStartActivityMemberAccess => SimpleMemberAccessExpression(ActivitySourceConditionalAccess, StartActivityIdentifierName);
-        
+        public static MemberAccessExpressionSyntax
+            SimpleMemberAccessExpression(ExpressionSyntax expression, SimpleNameSyntax name) =>
+            SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, expression, name);
+
+        public static readonly MemberAccessExpressionSyntax ActivityCurrentMemberAccess =
+            SimpleMemberAccessExpression(ActivityIdentifierName, CurrentIdentifierName);
+
+        public static readonly ConditionalAccessExpressionSyntax ActivitySourceConditionalAccess =
+            SyntaxFactory.ConditionalAccessExpression(ActivityCurrentMemberAccess,
+                SyntaxFactory.MemberBindingExpression(SourceIdentifierName));
+
+        public static readonly MemberAccessExpressionSyntax ActivityStartActivityMemberAccess =
+            SimpleMemberAccessExpression(ActivitySourceConditionalAccess, StartActivityIdentifierName);
+
         // Statements and Clauses
-        public static BlockSyntax EmptyBlock => SyntaxFactory.Block();
-        public static StatementSyntax ThrowStatement => SyntaxFactory.ThrowStatement();
-        public static CatchDeclarationSyntax ExceptionCatchDeclaration => SyntaxFactory.CatchDeclaration(ExceptionTypeName, SyntaxFactory.Identifier("exception"));
-        public static CatchClauseSyntax ExceptionCatchClause(BlockSyntax block) => SyntaxFactory.CatchClause().WithDeclaration(ExceptionCatchDeclaration).WithBlock(block);
-        public static ExpressionStatementSyntax ActivitySetStatusErrorStatement => SyntaxFactory.ExpressionStatement(SyntaxFactory.ParseExpression(ObservatorConstants.ActivitySetStatusError));
-        public static ExpressionStatementSyntax ActivitySetStatusOkStatement => SyntaxFactory.ExpressionStatement(SyntaxFactory.ParseExpression(ObservatorConstants.ActivitySetStatusOk));
-        public static ParameterSyntax ThisSourceParameter(string typeName) => SyntaxFactory.Parameter(SyntaxFactory.Identifier("@source")).AddModifiers(ThisKeyword).WithType(SyntaxFactory.ParseTypeName(typeName));
+        public static readonly StatementSyntax ThrowStatement = SyntaxFactory.ThrowStatement();
+
+        public static readonly CatchDeclarationSyntax ExceptionCatchDeclaration =
+            SyntaxFactory.CatchDeclaration(ExceptionTypeName, SyntaxFactory.Identifier("exception"));
+
+        public static CatchClauseSyntax ExceptionCatchClause(BlockSyntax block) => SyntaxFactory.CatchClause()
+            .WithDeclaration(ExceptionCatchDeclaration).WithBlock(block);
+
+        public static readonly ExpressionStatementSyntax ActivitySetStatusErrorStatement =
+            SyntaxFactory.ExpressionStatement(
+                SyntaxFactory.ParseExpression(ObservatorConstants.ActivitySetStatusError));
+
+        public static readonly ExpressionStatementSyntax ActivitySetStatusOkStatement =
+            SyntaxFactory.ExpressionStatement(SyntaxFactory.ParseExpression(ObservatorConstants.ActivitySetStatusOk));
+
+        private static ParameterSyntax ThisSourceParameterBase =
+            SyntaxFactory.Parameter(SyntaxFactory.Identifier("@source")).AddModifiers(ThisKeyword);
+
+        public static ParameterSyntax ThisSourceParameter(string typeName) =>
+            ThisSourceParameterBase.WithType(SyntaxFactory.ParseTypeName(typeName));
     }
 }
