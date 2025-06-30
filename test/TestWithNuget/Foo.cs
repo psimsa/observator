0d0a7be7b66e96ecdc03ba13d27cff35435ca63e
Pavel Simsa <pavel@simsa.cz>
2025-06-30 10:50:44 +0100

    wip

---
+++ test/TestWithNuget/Foo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Observator;
+
+namespace TestWithNuget;
+
+internal class Foo
+{
+    public Foo()
+    {
+        TestMethod();
+    }
+
+    [ObservatorTrace]
+
+    public void TestMethod()
+    {
+        Console.WriteLine("TestMethod executed.");
+    }
+}
